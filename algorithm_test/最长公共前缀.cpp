1c7b2c5e41e0954bb2f34cd3b0289932e7fde4ee
胡佳露 <974520639@qq.com>
2019-11-20 17:11:17 +0800

    the way of second

--- algorithm_test/最长公共前缀.cpp
+++ algorithm_test/最长公共前缀.cpp
@@ -48,3 +48,24 @@ public:
 };
 
 
+/*class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0){
+            return "";
+        }
+        if(strs.size()==1){
+            return strs[0];
+        }
+        int cow = strs[0].length();
+        for(int i=0; i<cow; i++){
+            for(int j=1; j<strs.size(); j++){
+                if(strs[j][i] != strs[0][i] || strs[j].length()==i){
+                    return strs[0].strs[0](0,i);
+                }
+            }
+        }
+    }
+};*/
+
+
