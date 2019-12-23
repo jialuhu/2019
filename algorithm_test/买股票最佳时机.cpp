9d8620ece3439ffe1a7a1f2b549e7d9e852c2e09
胡佳露 <974520639@qq.com>
2019-12-23 20:52:53 +0800

    最高峰以及最低峰值

---
+++ algorithm_test/买股票最佳时机.cpp
@@ -0,0 +1,53 @@
+/*************************************************************************
+	> File Name: 买股票最佳时机.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 一 12/23 20:51:52 2019
+ ************************************************************************/
+
+/**
+ 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ 
+ 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ 
+ 注意你不能在买入股票前卖出股票。
+ 
+ 示例 1:
+ 
+ 输入: [7,1,5,3,6,4]
+ 输出: 5
+ 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ 示例 2:
+ 
+ 输入: [7,6,4,3,1]
+ 输出: 0
+ 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ 
+ **/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = 0;
+        int sell = 0;
+        int day = prices.size();
+        if(day == 0 || day==1){
+            return 0;
+        }
+        buy = prices[0];
+        sell = prices[0];
+        int m = 0;
+        for(int i=1; i<day; i++){
+            if(prices[i] < buy && i!=day-1){
+                buy = prices[i];
+            }else if(prices[i]-buy>m){
+                m = prices[i]-buy;
+            }
+        }
+        return m;
+    }
+};
