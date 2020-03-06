c8c879d6c77c82cf6df11b668ac13cfd762353f9
胡佳露 <974520639@qq.com>
2020-03-06 18:36:13 +0800

    Singleton

--- algorithm_test/Singleton.cpp
+++ algorithm_test/Singleton.cpp
@@ -6,6 +6,7 @@
  ************************************************************************/
 
 #include<iostream>
+#include<mutex>
 using namespace std;
 //饿汉模式,线程安全
 class Singleton{
@@ -26,10 +27,44 @@ public:
         return mysingleton;
     }
 };
-std::shared_ptr<Singleton> Singleton::mysingleton(new Singleton());
+class SingletonLazy{
+private:
+    SingletonLazy(){
+        std::cout << "Singleton is Created\n";
+    }
+    //禁止生成拷贝函数
+    SingletonLazy(const Singleton&) = delete;
+    //禁止生成赋值函数
+    SingletonLazy& operator=(const SingletonLazy&) = delete;
+    static std::shared_ptr<SingletonLazy> mysingletonlazy;
+    static std::mutex mymutex;
+public:
+    ~SingletonLazy(){
+        std::cout << "SingletonLazy is destoied.\n";
+    }
+    static std::shared_ptr<SingletonLazy> getSingletonLazy(){
+        if(mysingletonlazy==nullptr){
+            std::lock_guard<std::mutex> lk(mymutex);
+            if(mysingletonlazy == nullptr){
+                mysingletonlazy = std::shared_ptr<SingletonLazy>(new SingletonLazy());
+            }
+        }
+        return mysingletonlazy;
+    }
+
+};
+/*std::shared_ptr<Singleton> Singleton::mysingleton(new Singleton());
 int main(){
     shared_ptr<Singleton> c1 = Singleton::getSingleton();
     shared_ptr<Singleton> c2 = Singleton::getSingleton();
     shared_ptr<Singleton> c3 = Singleton::getSingleton();
+    return 0;
+}*/
+std::shared_ptr<SingletonLazy> SingletonLazy::mysingletonlazy = nullptr;
+std::mutex SingletonLazy::mymutex;
+int main(){
+    std::shared_ptr<SingletonLazy> c1 = SingletonLazy::getSingletonLazy();
+    std::shared_ptr<SingletonLazy> c2 = SingletonLazy::getSingletonLazy();
+    std::shared_ptr<SingletonLazy> c3 = SingletonLazy::getSingletonLazy();
     return 0;
 }
