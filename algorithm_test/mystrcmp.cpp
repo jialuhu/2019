f42aa1ffed490737bfbd3cb1959c1a1213786f0e
胡佳露 <974520639@qq.com>
2020-03-06 11:42:36 +0800

    mystrcmp

--- algorithm_test/mystrcmp.cpp
+++ algorithm_test/mystrcmp.cpp
@@ -6,4 +6,19 @@
  ************************************************************************/
 
 #include<iostream>
-
+#include<assert.h>
+int mystrcmp(const char* s, const char* t){
+    assert(s != NULL && t != NULL);
+    while(*s && *t && *s==*t){
+        s++;
+        t++;
+    }
+    return((*s) - (*t));
+}
+int main(){
+    const char *s = "hello";
+    const char *t = "hello";
+    int diff = mystrcmp(s,t);
+    std::cout << diff << std::endl;
+    return 0;
+}
